91b2ee3a7ff2d2d612b48473eac771ac79520600
NEDSPC\Ned <grady.ned@gmail.com>
2019-07-01 23:28:17 +0100

    Added test project

---
+++ SqlCom.Test/ComViable.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System.Configuration;
+
+namespace SqlCom.Test
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			
+			Assert.Pass();
+		}
+	}
+}
---
+++ SqlCom.Test/DbTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace SqlCom.Test
+{
+	
+	class DbTests
+	{
+		[TestCaseSource(typeof(Setup), nameof(Setup.TestDbConnectionStringEnumerable))]
+		public static async Task CanConnectAndDisconnect(string connStr)
+		{
+			using (SqlConnection connection = new SqlConnection(connStr))
+			{
+				await connection.OpenAsync();
+
+				Assert.IsTrue(connection.State == ConnectionState.Open, $"Couldn't connect to {connStr}.");
+
+				connection.Close();
+				Assert.IsTrue(connection.State == ConnectionState.Closed, $"Couldn't disconnect from {connStr}.");
+			}
+		}
+
+		[TestCaseSource(typeof(Setup), nameof(Setup.TestDbConnectionStringEnumerable))]
+		public static async Task CanRunStoredProc(string connStr)
+		{
+			using (SqlConnection connection = new SqlConnection(connStr))
+			using (SqlCommand command = new SqlCommand("sp_who2", connection)
+			{
+				CommandType = CommandType.StoredProcedure
+			})
+			{
+				await connection.OpenAsync();
+				var result = await command.ExecuteReaderAsync();
+				Assert.IsTrue(result.HasRows);
+			}
+
+		}
+	}
+}
